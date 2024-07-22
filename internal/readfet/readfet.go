0a9b9104ff50e8b68fb5be860fa612d3666d2bd7
gradgrind <mt.bothfeld@gmail.com>
2024-07-22 12:53:51 +0200

    Start working on reading fet results

---
+++ internal/readfet/readfet.go
@@ -0,0 +1,98 @@
+package readfet
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+//	type Activity struct {
+//		Id       int
+//		Duration int
+//		Id365    string `xml:"Comments"`
+//	}
+type Activity struct {
+	//XMLName xml.Name `xml:"Activity"`
+	Id int
+	//	Teacher           []string
+	//	Subject           string
+	//	Students          []string
+	//	Active            bool
+	//	Total_Duration    int
+	Duration int
+	//	Activity_Group_Id int
+	Id365 string `xml:"Comments"`
+}
+
+type Activities_List struct {
+	//XMLName  xml.Name `xml:"Activities_List"`
+	Activity []Activity
+}
+
+type ConstraintActivityPreferredStartingTime struct {
+	//XMLName            xml.Name `xml:"ConstraintActivityPreferredStartingTime"`
+	Activity_Id        int
+	Preferred_Day      string
+	Preferred_Hour     string
+	Permanently_Locked bool
+}
+
+type StartingTime struct {
+	XMLName            xml.Name `xml:"ConstraintActivityPreferredStartingTime"`
+	Activity_Id        int
+	Preferred_Day      string
+	Preferred_Hour     string
+	Permanently_Locked bool
+}
+
+type ConstraintActivityPreferredRoom struct {
+	//XMLName     xml.Name `xml:"ConstraintActivityPreferredRoom"`
+	Activity_Id int
+	Room        string
+	//Permanently_Locked bool
+}
+
+type Time_Constraints_List struct {
+	//XMLName       xml.Name `xml:"Time_Constraints_List"`
+	ConstraintActivityPreferredStartingTime []ConstraintActivityPreferredStartingTime
+}
+
+type Space_Constraints_List struct {
+	//XMLName    xml.Name `xml:"Space_Constraints_List"`
+	ConstraintActivityPreferredRoom []ConstraintActivityPreferredRoom
+}
+
+type Result struct {
+	XMLName                xml.Name `xml:"fet"`
+	YearId365              string   `xml:"Comments"`
+	Activities_List        Activities_List
+	Time_Constraints_List  Time_Constraints_List
+	Space_Constraints_List Space_Constraints_List
+}
+
+func to_w365(fetpath string) {
+	// Open the  XML file
+	xmlFile, err := os.Open(fetpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the program
+	defer xmlFile.Close()
+	// read the opened XML file as a byte array.
+	byteValue, _ := io.ReadAll(xmlFile)
+	log.Printf("*+ Reading: %s\n", fetpath)
+	v := Result{}
+	err = xml.Unmarshal(byteValue, &v)
+	if err != nil {
+		log.Fatalf("XML error in %s:\n %v\n", fetpath, err)
+	}
+
+	fmt.Printf(" --- Year-Id: %s\n", v.YearId365)
+	fmt.Printf(" --- Activities_List:\n%+v\n", v.Activities_List)
+	fmt.Printf(" --- Time_Constraints_List:\n%+v\n",
+		v.Time_Constraints_List)
+	fmt.Printf(" --- Space_Constraints_List:\n%+v\n",
+		v.Space_Constraints_List)
+}
---
+++ internal/readfet/readfet_test.go
@@ -0,0 +1,16 @@
+package readfet
+
+import (
+	"log"
+	"path/filepath"
+	"testing"
+)
+
+func TestToW365(t *testing.T) {
+	fpath := "../_testdata/testx_data_and_timetable.fet"
+	abspath, err := filepath.Abs(fpath)
+	if err != nil {
+		log.Fatalf("Couldn't resolve file path: %s\n", fpath)
+	}
+	to_w365(abspath)
+}
